de4ff9fccf47b77334790dd8b22448e4135bab5f
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-22 16:56:05 +0200

    setup health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::Json, routing::get};
+use serde_json::{Value, json};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health", get(health_check));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> Json<Value> {
+    Json(json!({
+        "status": "ok"
+    }))
 }
