400e4dcb811fadf864ebe7bd0a3da456c23e5361
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-27 12:41:13 +0100

    add user routes

--- src/axummain/router.rs
+++ src/axummain/router.rs
@@ -1,10 +1,18 @@
+use axum::routing::post;
 use axum::{Router, routing::get};
 
 use crate::axummain::state::AppState;
+use crate::handlers::auth::{current_user, login, register};
 use crate::handlers::server_health::server_health_check;
 
 pub fn get_main_router(app_state: AppState) -> Router {
     Router::new()
+        // Health check route
         .route("/health", get(server_health_check))
+        // Auth routes
+        .route("/api/users", post(register))
+        .route("/api/users/login", post(login))
+        .route("/api/user", get(current_user))
+        // Set application state
         .with_state(app_state)
 }
