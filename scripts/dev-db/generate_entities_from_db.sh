6c53ef2c9112a70fff8f99fa27425f92bc33173a
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-28 18:48:12 +0100

    script to generate new entities

---
+++ scripts/dev-db/generate_entities_from_db.sh
@@ -0,0 +1,4 @@
+DATABASE_URL="postgres://dev:dev-db@localhost:5432/dimdimhealthdev"
+
+sea-orm-cli migrate up --database-url $DATABASE_URL -v
+sea-orm-cli generate entity --database-url $DATABASE_URL --output-dir src/entities  --with-serde "both" -v
