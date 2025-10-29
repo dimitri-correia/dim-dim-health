cc07d72d7d84743ecdeebcae2bc5ff6e66cbe558
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-29 16:11:49 +0100

    adapt to sea orm

--- src/handlers/server_health.rs
+++ src/handlers/server_health.rs
@@ -4,15 +4,8 @@ use serde_json::{Value, json};
 use crate::axummain::state::AppState;
 
 pub async fn server_health_check(State(state): State<AppState>) -> Json<Value> {
-    match sqlx::query("SELECT 1 AS value").fetch_one(&state.db).await {
-        Ok(_) => Json(json!({
-            "status": "ok"
-        })),
-        Err(e) => {
-            eprintln!("Database error: {}", e);
-            Json(json!({
-                "status": "error",
-            }))
-        }
+    match &state.db.ping().await {
+        Ok(_) => Json(json!({"status": "ok"})),
+        Err(_) => Json(json!({"status": "error"})),
     }
 }
