003311648d1c8bd329a3f1b2a87e9622e92aced7
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-29 21:38:19 +0100

    writing tests for password

--- src/auth/password.rs
+++ src/auth/password.rs
@@ -1,9 +1,40 @@
 use bcrypt::{DEFAULT_COST, hash, verify};
 
-pub fn hash_password(password: &str) -> Result<String, bcrypt::BcryptError> {
-    hash(password, DEFAULT_COST)
+pub fn hash_password(password: &str, cost: Option<u32>) -> Result<String, bcrypt::BcryptError> {
+    let cost = cost.unwrap_or(DEFAULT_COST);
+    hash(password, cost)
 }
 
 pub fn verify_password(password: &str, hash: &str) -> Result<bool, bcrypt::BcryptError> {
     verify(password, hash)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash_and_verify_password() {
+        let password = "my_secret_pwd";
+
+        let hashed = hash_password(password, Some(4)).expect("failed to hash password");
+
+        assert_ne!(password, hashed);
+
+        let is_valid = verify_password(password, &hashed).expect("failed to verify password");
+
+        assert!(is_valid);
+    }
+
+    #[test]
+    fn test_verify_password_incorrect() {
+        let password = "correct_pwd";
+        let wrong = "wrong_pwd";
+
+        let hashed = hash_password(password, Some(4)).expect("failed to hash password");
+
+        let is_valid = verify_password(wrong, &hashed).expect("failed to verify password");
+
+        assert!(!is_valid);
+    }
 }
