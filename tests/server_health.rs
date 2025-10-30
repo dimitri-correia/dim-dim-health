231d8b5de5f03b1f247f5075a85c547ba857b626
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-30 16:14:17 +0100

    test server health

---
+++ tests/server_health.rs
@@ -0,0 +1,23 @@
+mod common;
+use axum::http::StatusCode;
+use common::get_test_server;
+use serde_json::json;
+
+#[tokio::test]
+async fn test_server_health() {
+    let server = get_test_server().await;
+    let res = server.get("/health").await;
+    res.assert_status(StatusCode::OK);
+    res.assert_json(&json!(
+        {
+            "status":"ok"
+        }
+    ));
+}
+
+#[tokio::test]
+async fn test_bad_route() {
+    let server = get_test_server().await;
+    let res = server.get("/non_existing_route").await;
+    res.assert_status(StatusCode::NOT_FOUND);
+}
