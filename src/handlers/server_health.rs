2982c5072576603d1d624e81aa07db2310d4b5d1
Dimitri CORREIA <dimitri.correia@proton.me>
2025-10-30 16:13:48 +0100

    return status code

--- src/handlers/server_health.rs
+++ src/handlers/server_health.rs
@@ -1,11 +1,15 @@
-use axum::{Json, extract::State};
-use serde_json::{Value, json};
+use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+use serde_json::json;
 
 use crate::axummain::state::AppState;
 
-pub async fn server_health_check(State(state): State<AppState>) -> Json<Value> {
+pub async fn server_health_check(State(state): State<AppState>) -> impl IntoResponse {
     match &state.db.ping().await {
-        Ok(_) => Json(json!({"status": "ok"})),
-        Err(_) => Json(json!({"status": "error"})),
+        Ok(_) => (StatusCode::OK, Json(json!({"status": "ok"}))).into_response(),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"status": "error"})),
+        )
+            .into_response(),
     }
 }
