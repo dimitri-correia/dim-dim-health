0907d204fcf717db40e7631ba7b94d492d4de428
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-01 13:36:52 +0100

    sea-orm-cli migrate generate create_email_verification_tokens

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20251028_170454_user_table;
+mod m20251101_123625_create_email_verification_tokens;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20251028_170454_user_table::Migration)]
+        vec![
+            Box::new(m20251028_170454_user_table::Migration),
+            Box::new(m20251101_123625_create_email_verification_tokens::Migration),
+        ]
     }
 }
