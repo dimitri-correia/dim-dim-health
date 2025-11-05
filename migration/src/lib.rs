82f39bfe8a03b04597903e930624da34ae8bf02e
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-05 18:53:31 +0100

    add pgcron to delete expired token

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20251028_170454_user_table;
 mod m20251101_123625_create_email_verification_tokens;
 mod m20251101_124651_add_email_verified_to_users;
+mod m20251105_173226_pg_cron_delete_expired_token;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20251028_170454_user_table::Migration),
             Box::new(m20251101_123625_create_email_verification_tokens::Migration),
             Box::new(m20251101_124651_add_email_verified_to_users::Migration),
+            Box::new(m20251105_173226_pg_cron_delete_expired_token::Migration),
         ]
     }
 }
