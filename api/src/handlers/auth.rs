8391eb156eb751b6d120b05ed374beff756b17ba
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-10 16:51:02 +0100

    add email job to register handler

--- api/src/handlers/auth.rs
+++ api/src/handlers/auth.rs
@@ -5,9 +5,13 @@ use crate::{
         password::{hash_password, verify_password},
     },
     axummain::state::AppState,
+    jobs::email::send_register_email,
     schemas::auth_schemas::*,
+    utils::token_generator::generate_verification_token,
 };
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+use chrono::{Duration, FixedOffset, Utc};
+use log::error;
 use serde_json::json;
 use validator::Validate;
 
@@ -40,6 +44,31 @@ pub async fn register(
         .create(&payload.user.username, &payload.user.email, &password_hash)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    let verification_token = generate_verification_token();
+    let offset = FixedOffset::east_opt(0).unwrap();
+    let expires_at = Utc::now().with_timezone(&offset) + Duration::days(2);
+
+    if let Err(err) = state
+        .email_verification_repository
+        .create_token(&user.id, &verification_token, &expires_at)
+        .await
+    {
+        error!("Failed to register token because: {err}");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR.into_response());
+    }
+
+    if let Err(err) = send_register_email(
+        &user.email,
+        &user.username,
+        &verification_token,
+        state.redis,
+    )
+    .await
+    {
+        error!("err: {err}");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR.into_response());
+    }
 
     let token = generate_token(&user.id, &state.jwt_secret)
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
