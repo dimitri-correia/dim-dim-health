0cf55e8b52c2e29ac308701d8461d2ca385e73ae
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-14 12:27:33 +0100

    expire in 2 hours instead of 2 days

--- worker/src/mail_jobs/register_mail.rs
+++ worker/src/mail_jobs/register_mail.rs
@@ -14,7 +14,7 @@ pub async fn handle_registration_email(
         worker_state.base_url, data.token
     );
     let content = format!(
-        "Hey {}.\n Thanks for registering!\n Please verify your email by clicking the following link: {verification_link}",
+        "Hey {}.\nThanks for registering!\nPlease verify your email by clicking the following link: {verification_link} (this link will expire in 2 hours)\n\n Cheers,\n DimDim Health",
         data.username
     );
 
