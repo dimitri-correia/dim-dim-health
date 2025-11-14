3c37dfcf2e36f1c2d10276861722e7c194a54ed1
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-15 00:47:07 +0100

    auto delete expired pass reset tokens

---
+++ migration/src/m20251114_234540_pg_cron_delete_expired_token.rs
@@ -0,0 +1,38 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+static CRON_NAME: &str = "cleanup_expired_pass_reset_tokens";
+static PASSWORD_RESET_TABLE: &str = "password_reset_token";
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .get_connection()
+            .execute_unprepared(&format!(
+                r#"
+                CREATE EXTENSION IF NOT EXISTS pg_cron;
+                 SELECT cron.schedule(
+                    '{CRON_NAME}',
+                    '0 * * * *',
+                    'DELETE FROM {PASSWORD_RESET_TABLE} WHERE expires_at < NOW()'
+                );
+                "#,
+            ))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .get_connection()
+            .execute_unprepared(&format!("SELECT cron.unschedule('{CRON_NAME}');"))
+            .await
+            .ok(); // ignore errors if not exists
+
+        Ok(())
+    }
+}
