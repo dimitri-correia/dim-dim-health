743e0cd7d1f05003f898dab460275fd7086f2c50
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-19 18:37:37 +0100

    clean after 5 days if not verified
    we want to keep it more because we want to be able to resend verification link - TODO

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -15,6 +15,7 @@ mod m20251115_160046_create_food_item;
 mod m20251115_160750_create_meal;
 mod m20251115_161449_create_meal_item;
 mod m20251119_163331_pg_cron_clean_guest_users;
+mod m20251119_172747_pg_cron_clean_non_verified_email;
 
 pub struct Migrator;
 
@@ -37,6 +38,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20251115_160750_create_meal::Migration),
             Box::new(m20251115_161449_create_meal_item::Migration),
             Box::new(m20251119_163331_pg_cron_clean_guest_users::Migration),
+            Box::new(m20251119_172747_pg_cron_clean_non_verified_email::Migration),
         ]
     }
 }
