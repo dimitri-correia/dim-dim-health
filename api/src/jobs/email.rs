bc1d9e00b7e2c253cfb417a9377af884941da163
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-24 14:19:24 +0100

    add new mail to change email address

--- api/src/jobs/email.rs
+++ api/src/jobs/email.rs
@@ -64,4 +64,31 @@ impl EmailJob {
         con.rpush::<_, _, ()>("jobs", serde_json::to_string(&job).unwrap())
             .await
     }
+
+    pub async fn send_email_change_email(
+        &self,
+        email: &str,
+        username: &str,
+        token: &str,
+    ) -> Result<(), redis::RedisError> {
+        let job_email_change = JobEmailRegister {
+            email: email.to_string(),
+            username: username.to_string(),
+            token: token.to_string(),
+        };
+
+        let job_email = JobEmail {
+            email_type: EmailType::EmailChange,
+            data: serde_json::to_value(job_email_change).unwrap(),
+        };
+
+        let job = Job {
+            task_type: TaskType::Email,
+            data: serde_json::to_value(job_email).unwrap(),
+        };
+
+        let mut con = self.redis.clone();
+        con.rpush::<_, _, ()>("jobs", serde_json::to_string(&job).unwrap())
+            .await
+    }
 }
