7eec0aea7bed0947424abe604082f85608077eff
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-28 15:32:25 +0100

    change config logic

--- entities/src/env_loader.rs
+++ entities/src/env_loader.rs
@@ -41,14 +41,14 @@ pub struct Settings {
 
 impl Settings {
     pub fn load_config() -> Result<Self, config::ConfigError> {
-        // Determine the environment (default to "dev" if not set)
         let env = std::env::var("APP_ENV").unwrap_or_else(|_| "dev".into());
 
         let builder = Config::builder()
-            // Load common first
+            // Load env-specific file first
+            .add_source(File::with_name(&format!("config/{}.toml", env)).required(true))
+            // Override with common settings
             .add_source(File::with_name("config/common.toml").required(true))
-            // Override with env-specific file
-            .add_source(File::with_name(&format!("config/{}.toml", env)).required(false))
+            // Add the environment variable to the configuration
             .set_default("env", env)?;
 
         builder.build()?.try_deserialize()
