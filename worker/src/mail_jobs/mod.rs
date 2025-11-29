41d319e02a57942a78f3e7b7ab35e14ff25a0e93
Dimitri CORREIA <dimitri.correia@proton.me>
2025-11-30 00:57:31 +0100

    add user recaps

--- worker/src/mail_jobs/mod.rs
+++ worker/src/mail_jobs/mod.rs
@@ -2,3 +2,6 @@ pub mod common_mail_jobs;
 pub mod email_change_mail;
 pub mod register_mail;
 pub mod reset_password_mail;
+pub mod monthly_recap_mail;
+pub mod weekly_recap_mail;
+pub mod yearly_recap_mail;
